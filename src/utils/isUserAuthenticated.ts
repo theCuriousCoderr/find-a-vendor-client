cd7cbb4cc4c039dd63df89bafd70a5c40a052d2c
Olalekan Oladimeji <elijahdimeji549@gmail.com>
2025-04-19 20:11:22 +0100

    feat: change cookie to localStorage 2

--- src/utils/isUserAuthenticated.ts
+++ src/utils/isUserAuthenticated.ts
@@ -1,12 +1,17 @@
-export default function isUserAuthenticated() {
-  const customer_id = localStorage.getItem("customer_id");
-  const vendor_id = localStorage.getItem("vendor_id");
+"use client";
 
-  if (customer_id) {
-    return { customer_id: customer_id };
-  }
-  if (vendor_id) {
-    return { vendor_id: vendor_id };
+export default function isUserAuthenticated() {
+  if (typeof window !== "undefined") {
+    const customer_id = localStorage.getItem("customer_id");
+    const vendor_id = localStorage.getItem("vendor_id");
+
+    if (customer_id) {
+      return { customer_id: customer_id };
+    }
+    if (vendor_id) {
+      return { vendor_id: vendor_id };
+    }
+    return null;
   }
   return null;
 }
